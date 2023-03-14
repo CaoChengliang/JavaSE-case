d6d5912ab6f62d1958f980cdd719fd16d01010c0
CaoChengliang <1076547515@qq.com>
2023-03-14 19:03:17 +0800

    "模拟斗地主游戏，实现了发牌，洗牌，排序"

---
+++ doudizhu/src/com/learnJavaSE/doudizhu/Card.java
@@ -0,0 +1,67 @@
+package com.learnJavaSE.doudizhu;
+
+public class Card implements Comparable{
+	
+	private String size;//点数
+	private String color;//花色
+	private int index;   //牌的大小
+	
+	public Card() {
+		
+	}
+	
+	
+	public Card(String size, String color,int index) {
+		super();
+		this.size = size;
+		this.color = color;
+		this.index = index;
+	}
+	public String getSize() {
+		return size;
+	}
+	public void setSize(String size) {
+		this.size = size;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	
+	public int getIndex() {
+		return index;
+	}
+
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+
+	@Override
+	public String toString() {
+		
+		return size+color;
+				
+	}
+	
+	@Override
+	public int compareTo( Object o ) {
+		
+		Card c = (Card) o;
+		
+		if(this.index == c.index) {
+			return 0;
+		}
+		
+		return this.index > c.index ? 1:-1;
+		
+	}
+	
+	
+	
+
+}
---
+++ doudizhu/src/com/learnJavaSE/doudizhu/GamePanel.java
@@ -0,0 +1,114 @@
+package com.learnJavaSE.doudizhu;
+
+import java.util.*;
+
+/**
+ * @author alan
+ * 
+ * 功能：
+ *   1.做牌
+ *   2.洗牌
+ *   3.定义三个玩家
+ *   3.发牌
+ *   5.排序
+ *   6.看牌
+ *
+ */
+public class GamePanel {
+	/*
+	 * 1.做牌定义一个静态集合存出54张牌
+	 * 2.定义静态代码块初始化牌数据
+	 * 
+	 * */
+   public static List<Card> allCards = new ArrayList<>();
+   
+   static {
+	   
+	   String[] size = new String[] {"3","4","5","6","7","8","9","10","J","Q","K","A","2"};
+	   String[] color = new String[] {"♠","♣","♥","✦"};
+	   int count = 1;   //排数大小
+	   for(String s:size) {//加牌
+		   for(String c:color) {
+			   
+			   Card card = new Card();
+			   
+			   card.setIndex(count);
+			   card.setSize(s);
+			   card.setColor(c);
+			   
+			   allCards.add(card);
+			   
+			   
+		   }
+		   count++;
+	   }
+	   
+	   Card c1 = new Card("" , "♔",100);
+	   Card c2 = new Card("" , "♕",99);
+	   
+	   Collections.addAll(allCards , c1, c2);
+	   
+	   System.out.println("新牌"+allCards);
+   }
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * 洗牌
+		 */
+		Collections.shuffle(allCards);
+		System.out.println("洗牌后："+allCards);
+		
+		
+		/* 发牌 
+		 * */
+		//创建三个人对象
+		List<Card> xiaocao = new ArrayList<>();
+		List<Card> xiaoli = new ArrayList<>();
+		List<Card> xiaolu = new ArrayList<>();
+		
+		for(int i = 0 ; i < allCards.size()-3 ; i++ ) {
+			
+			if(i % 3 == 0) {
+				
+				xiaocao.add(allCards.get(i));
+				
+			}else if(i % 3 == 1) {
+				
+				xiaoli.add(allCards.get(i));
+				
+			}else if(i % 3 == 2){
+				
+				xiaolu.add(allCards.get(i));
+			}
+			
+		}
+		
+		//每个人的从小到大排序
+		
+		Collections.sort(xiaocao);
+		Collections.sort(xiaoli);
+		Collections.sort(xiaolu);
+		//最后三张牌
+		List<Card> lastthreeCard = allCards.subList(allCards.size()-3 , allCards.size() );
+		
+		System.out.println("xiaocao"+xiaocao);
+		System.out.println("xiaoli"+xiaoli);
+		System.out.println("xiaolu"+xiaolu);
+		System.out.println("lastthreeCards"+lastthreeCard);
+		
+		for (Card card : lastthreeCard) {
+			xiaocao.add(card);
+		}
+		Collections.sort(xiaocao);
+		System.out.println("xiaocao"+xiaocao);
+		System.out.println("xiaoli"+xiaoli);
+		System.out.println("xiaolu"+xiaolu);
+		
+		
+	
+	}
+
+}
